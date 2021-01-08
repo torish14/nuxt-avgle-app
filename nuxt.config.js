9bb90540dd73af5aab6e2a59459a8477f4929e87
toshihirokato <nanamin.104@icloud.com>
2021-01-09 05:49:05 +0900

    minimal-ui の削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
-const URL = 'http://localhost:3000'
+const Url = 'http://localhost:3000'
 
-const baseHost = process.env.BASE_HOST || URL
+const baseHost = process.env.BASE_HOST || Url
 const baseDir = process.env.BASE_DIR || '/'
 const baseUrl = baseHost + baseDir
 
@@ -24,7 +24,7 @@ export default {
   head: {
     htmlAttrs: {
       lang,
-      prefix: 'og: http://ogp.me/ns#'
+      prefix: 'og: http://ogp.me/ns#',
     },
     // titleTemplate: `%s - ${siteName}` + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
@@ -33,50 +33,53 @@ export default {
       { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1.0, user-scalable=no, minimal-ui'
+        content: 'width=device-width, initial-scale=1.0, user-scalable=no',
       },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || siteDesc
+        content: process.env.npm_package_description || siteDesc,
       },
       {
         hid: 'keywords',
         name: 'keywords',
-        content: siteKeywords
+        content: siteKeywords,
       },
       {
         hid: 'og:site_name',
         property: 'og:site_name',
-        content: siteName
+        content: siteName,
       },
       {
         hid: 'og:type',
         property: 'og:type',
-        content: 'website'
+        content: 'website',
       },
       {
         hid: 'og:url',
         property: 'og:url',
-        content: baseUrl
+        content: baseUrl,
       },
       {
         hid: 'og:title',
         property: 'og:title',
-        content: siteName
+        content: siteName,
       },
       {
         hid: 'og:description',
         property: 'og:description',
-        content: siteDesc
+        content: siteDesc,
       },
       // {
       //   hid: "og:image",
       //   property: "og:image",
       //   content: `${ogpImages}home.jpg`
       // },
-      { name: 'twitter:card', content: 'summary_large_image' }
-    ]
+      { name: 'twitter:card', content: 'summary_large_image' },
+    ],
+    // link: [{
+    //   rel: 'stylesheet', href: 'https://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.css'
+    // }],
     // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
@@ -85,7 +88,7 @@ export default {
   // loading: { color: '#fff' },
   loading: {
     color: '#6b46c1',
-    continuous: true
+    continuous: true,
   },
   /*
    ** Global CSS
@@ -100,7 +103,7 @@ export default {
     // { src: '~/plugins/lazyload.js', mode: 'client' },
     '~/plugins/axios.js',
     '~/plugins/sentry.js',
-    '~/plugins/pagination.js'
+    '~/plugins/pagination.js',
   ],
   router: {
     // middleware: ['authenticated']
@@ -114,7 +117,7 @@ export default {
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
     // Doc: https://google-analytics.nuxtjs.org
-    '@nuxtjs/google-analytics'
+    '@nuxtjs/google-analytics',
   ],
   /*
    ** Nuxt.js modules
@@ -123,7 +126,7 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     'nuxt-material-design-icons',
-    ['@nuxtjs/pwa', { icon: false }]
+    ['@nuxtjs/pwa', { icon: false }],
   ],
   workbox: {
     // 開発環境でもPWAできるように
@@ -153,13 +156,16 @@ export default {
     credentials: false,
     proxy: true,
     // baseURL: process.browser ? '' : process.env.API_HOST
-    baseURL: URL
+    baseURL: Url,
     // timeout: 3000
   },
   proxy: {
     '/api': {
-      target: URL
-    }
+      target: Url,
+      pathRewrite: {
+        '^/api/': '/',
+      },
+    },
   },
   /*
    ** Build configuration
@@ -168,23 +174,27 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend (config, ctx) {
-      config.performance = config.performance || {}
-      config.performance.maxEntrypointSize = 1200 * 1024
-      config.performance.maxAssetSize = 700 * 1024
+    extend(config, ctx) {
+      config.performance = config.performance || {};
+      config.performance.maxEntrypointSize = 1200 * 1024;
+      config.performance.maxAssetSize = 700 * 1024;
     },
     babel: {
-      presets ({ isServer }) {
+      presets({ isServer }) {
         return [
           [
             require.resolve('@nuxt/babel-preset-app'),
             {
               buildTarget: isServer ? 'server' : 'client',
-              corejs: { version: 3 }
-            }
-          ]
-        ]
-      }
+              corejs: { version: 3 },
+            },
+          ],
+        ];
+      },
+    },
+    filenames: {
+      app: ({ isDev }) => (isDev ? '[name].[hash].js' : '[chunkhash].js'),
+      chunk: ({ isDev }) => (isDev ? '[name].[hash].js' : '[chunkhash].js'),
     }
   }
-}
+};
