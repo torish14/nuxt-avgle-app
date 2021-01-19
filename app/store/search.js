fa16db159399eeee79579073b0d532f465b89ab9
toshihirokato <nanamin.104@icloud.com>
2021-01-20 08:35:47 +0900

    ローディング有りの場合のコード追記

--- app/store/search.js
+++ app/store/search.js
@@ -2,7 +2,8 @@ const searchUrl = 'https://api.avgle.com/v1/search/'
 
 export const state = () => ({
   message: '日本人',
-  messages: []
+  messages: [],
+  // isLoading: false
 })
 
 export const getters = {
@@ -11,16 +12,22 @@ export const getters = {
 }
 
 export const mutations = {
-  mutateMessage (state, payload) {
+  mutateMessage(state, payload) {
     state.message = payload
   },
   // ? 検索結果
-  setSearchItems (state, messages) {
+  setSearchItems(state, messages) {
     state.messages = messages
   },
-  clearMessage (state) {
+  clearMessage(state) {
     state.message = '日本人'
-  }
+  },
+  // hideLoading(state) {
+  //   state.isLoading = false
+  // },
+  // showLoading(state) {
+  //   state.isLoading = true
+  // },
 }
 
 export const actions = {
@@ -30,14 +37,42 @@ export const actions = {
   async getSearchItems ({ commit, state }) {
     // const obj = JSON.parse(localStorage.getItem('vuex'))
     // if (!obj.search.message) { obj.search.message = '日本人' }
-    const getSearchItemsResponse = await this.$axios
-      .$get(encodeURI(searchUrl + state.message + '/0' + '?limit=150' + '?type=public'))
-      .catch((err) => {
-        console.log(err)
-        this.$router.push('/error')
-      })
-    commit('setSearchItems', getSearchItemsResponse.response.videos)
 
-    console.log(getSearchItemsResponse)
+    // commit('showLoading')
+    const config = {
+      headers: { 'content-type': 'application/json' }
+    }
+    const getSearchItemsResponse = await this.$axios
+      .$get(encodeURI(searchUrl + state.message + '/0' + '?limit=150' + '?type=public'), config)
+      .catch((err) => {
+        if (err.response.status !== 403) {
+          // console.log(err)
+          // commit('hideLoading')
+          this.$router.push('/error')
+        }
+      })
+    // console.log(getSearchItemsResponse)
+    commit('setSearchItems', getSearchItemsResponse.response.videos)
+    // commit('hideLoading')
+
+    // try {
+    //   commit('showLoading')
+    //   const config = {
+    //     headers: { 'content-type': 'application/json' }
+    //   }
+    //   const getSearchItemsResponse = await this.$axios.$get(
+    //     encodeURI(
+    //       searchUrl + state.message + '/0' + '?limit=150' + '?type=public'
+    //     ),
+    //     config
+    //   )
+    //   console.log(getSearchItemsResponse)
+    //   commit('setSearchItems', getSearchItemsResponse.response.videos)
+    //   commit('hideLoading')
+    // } catch (err) {
+    //   console.log(err)
+    //   commit('hideLoading')
+    //   this.$router.push('/error')
+    // }
   }
 }
