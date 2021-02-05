5e7fc1ff1cb4a4bea7e6c31d3006f9204d32599b
toshihirokato <nanamin.104@icloud.com>
2021-02-05 22:01:20 +0900

    メールアドレスの追加

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,23 +1,7 @@
 <template>
   <!-- フッター -->
-  <footer class="text-gray-500 bg-black body-font">
-    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-      <!-- <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-          </svg>
-          <span class="ml-3 text-xl">avgle</span>
-        </a> -->
-      <!-- <nuxt-link to="/"> -->
+  <footer class="text-gray-600 bg-black body-font">
+    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col normal-footer">
       <a href="/" @click="changeForm">
         <img
           src="~/assets/Porngle ロゴ.jpeg"
@@ -25,24 +9,87 @@
           class="w-40"
         >
       </a>
-      <!-- </nuxt-link> -->
       <p class="text-sm text-gray-600 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
-        &copy; {{ new Date().getFullYear() }} - Porngle
+        &copy; 2020 - Porngle
+      </p>
+      <p class="text-xs text-gray-600 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
+        porngle.love@gmail.com
       </p>
     </div>
+    <!-- レスポンシブ -->
+    <!-- <div class="flex justify-around py-1 fixed z-10 bottom-0 bg-black w-full responsive">
+      <nuxt-link to="/">
+        <div class="text-center" :class="[ activetab === 1 ? 'active' : '' ]" @click="activetab=1">
+          <i class="material-icons">home</i>
+          <p class="icon-text">
+            ホーム
+          </p>
+        </div>
+      </nuxt-link>
+      <nuxt-link to="/suggest">
+        <div class="text-center" :class="[ activetab === 2 ? 'active' : '' ]" @click="activetab=2">
+          <i class="material-icons">live_tv</i>
+          <p class="icon-text">
+            オススメ
+          </p>
+        </div>
+      </nuxt-link>
+      <nuxt-link to="/search" @click.native="clearForm()">
+        <div class="text-center" :class="[ activetab === 3 ? 'active' : '' ]" @click="activetab=3">
+          <i class="material-icons">search</i>
+          <p class="icon-text">
+            検索
+          </p>
+        </div>
+      </nuxt-link>
+    </div> -->
   </footer>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      activetab: 1
+    }
+  },
   methods: {
     changeForm () {
+      this.$store.commit('search/changeMessage')
+    },
+    clearForm () {
       this.$store.commit('search/clearMessage')
+    },
+    focusSearch () {
+      this.$refs.textInput.focus()
     }
   },
 }
 </script>
 
-<style>
+<style scoped>
+/* @media screen and (max-width: 768px) {
+  .normal-footer {
+    display: none !important;
+  }
 
+  .material-icons {
+    font-size: 24px !important;
+  }
+
+  .icon-text {
+    font-size: 10px;
+  }
+
+  .active {
+    color: #6b46c1;
+    color: white;
+  }
+}
+
+@media screen and (min-width: 769px) {
+  .responsive {
+    display: none !important;
+  }
+} */
 </style>
