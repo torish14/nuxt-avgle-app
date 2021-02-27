c42318543b448beb813d996638e7af44a3b5d41c
toshihirokato <nanamin.104@icloud.com>
2021-02-27 09:48:46 +0900

    レイアウトの修正

--- components/Skeleton.vue
+++ components/Skeleton.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div>
-      <div class="bg-gray-800" style="width: 320px; height: 180px;" />
-      <p class="mt-2 bg-gray-800" style="width: 320px; height: 16px;" />
-      <p class="mt-2 bg-gray-800" style="width: 120px; height: 14px;" />
-      <p class="mt-2 mb-4 bg-gray-800" style="width: 160px; height: 14px;" />
+      <div class="img bg-gray-800" style="width: 320px; height: 180px;" />
+      <p class="title mt-2 bg-gray-800" style="width: 320px; height: 16px;" />
+      <p class="viewnumber mt-2 bg-gray-800" style="width: 120px; height: 14px;" />
+      <p class="keyword mt-2 mb-4 bg-gray-800" style="width: 160px; height: 14px;" />
     </div>
   </div>
 </template>
