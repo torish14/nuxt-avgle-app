d75620cac1c226539be8e5074576db93c1fea348
toshihirokato <nanamin.104@icloud.com>
2021-03-06 13:57:44 +0900

    @nuxtjs/device に記述変更

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,49 +1,57 @@
 <template>
-  <!-- フッター -->
-  <footer class="text-gray-600 bg-black body-font">
-    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col normal-footer">
-      <a href="https://nuxt-pornhub-api.web.app" aria-label="ホームへ戻る" @click="changeForm">
-        <img
-          src="~/assets/Porngle ロゴ.jpeg"
-          alt="ロゴ"
-          class="w-40"
-        >
-      </a>
-      <p class="text-sm text-gray-600 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
-        &copy; 2020 - Porngle
-      </p>
-      <p class="text-xs text-gray-600 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
-        porngle.love@gmail.com
-      </p>
+  <section>
+    <!-- デスクトップ -->
+    <div v-if="$device.isDesktop" class="bg-black">
+      <footer class="text-gray-600">
+        <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col normal-footer">
+          <a href="https://nuxt-pornhub-api.web.app" aria-label="ホームへ戻る" @click="changeForm">
+            <img
+              src="~/assets/Porngle ロゴ.jpeg"
+              alt="ロゴ"
+              class="w-40"
+            >
+          </a>
+          <p class="text-sm text-gray-600 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
+            &copy; 2020 - Porngle
+          </p>
+          <p class="text-xs text-gray-600 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
+            porngle.love@gmail.com
+          </p>
+        </div>
+      </footer>
     </div>
-    <!-- レスポンシブ -->
-    <!-- <div class="flex justify-around py-1 fixed z-10 bottom-0 bg-black w-full responsive">
-      <nuxt-link to="/">
-        <div class="text-center" :class="[ activetab === 1 ? 'active' : '' ]" @click="activetab=1">
-          <i class="material-icons">home</i>
-          <p class="icon-text">
-            ホーム
-          </p>
+    <!-- モバイル -->
+    <div v-else class="bg-black">
+      <footer class="text-gray-600">
+        <div class="flex justify-around py-1 fixed z-10 bottom-0 bg-black w-full">
+          <nuxt-link to="/">
+            <div class="text-center">
+              <i class="material-icons">home</i>
+              <p class="icon-text" style="font-size: 8px;">
+                ホーム
+              </p>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/suggest">
+            <div class="text-center">
+              <i class="material-icons">live_tv</i>
+              <p class="icon-text" style="font-size: 8px;">
+                オススメ
+              </p>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/search" @click.native="clearForm()">
+            <div class="text-center">
+              <i class="material-icons">search</i>
+              <p class="icon-text" style="font-size: 8px;">
+                検索
+              </p>
+            </div>
+          </nuxt-link>
         </div>
-      </nuxt-link>
-      <nuxt-link to="/suggest">
-        <div class="text-center" :class="[ activetab === 2 ? 'active' : '' ]" @click="activetab=2">
-          <i class="material-icons">live_tv</i>
-          <p class="icon-text">
-            オススメ
-          </p>
-        </div>
-      </nuxt-link>
-      <nuxt-link to="/search" @click.native="clearForm()">
-        <div class="text-center" :class="[ activetab === 3 ? 'active' : '' ]" @click="activetab=3">
-          <i class="material-icons">search</i>
-          <p class="icon-text">
-            検索
-          </p>
-        </div>
-      </nuxt-link>
-    </!-->
-  </footer>
+      </footer>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -64,7 +72,7 @@ export default Vue.extend({
   },
   methods: {
     changeForm () {
-      this.$accessor.search.changeMessage()
+      this.$accessor.search.setJapaneseMessage()
     },
     clearForm () {
       this.$accessor.search.clearMessage()
@@ -76,3 +84,9 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style>
+.nuxt-link-exact-active {
+  color: #e2e8f0;
+}
+</style>
