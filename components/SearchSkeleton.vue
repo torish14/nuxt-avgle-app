1e8446e8df6661018b9562ec7fae6c7e90fc474a
toshihirokato <nanamin.104@icloud.com>
2021-03-08 07:53:33 +0900

    search.vue用のスケルトンコンポーネント作成

---
+++ components/SearchSkeleton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="$device.isMobile">
+    <!-- <div class="flex"> -->
+    <div class="bg-gray-800" style="width: 170px; height: 96px;" />
+    <p class="mt-2 bg-gray-800 pl-2" style="width: 180px; height: 14px;" />
+    <!-- <div class="flex flex-row"> -->
+    <p class="mt-2 bg-gray-800 pl-2" style="width: 140px; height: 12px;" />
+    <p class="mt-2 mb-4 bg-gray-800 pl-2" style="width: 160px; height: 10px;" />
+    <!-- </div> -->
+    <!-- </div> -->
+  </div>
+</template>
