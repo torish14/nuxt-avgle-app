d7912f6bb6b41bf76ae8017e62a7fa6d37cbdb93
toshihirokato <nanamin.104@icloud.com>
2021-03-08 13:48:35 +0900

    レイアウト（横幅・高さ）の調整

--- components/Skeleton.vue
+++ components/Skeleton.vue
@@ -4,15 +4,15 @@
     <div v-if="$device.isDesktop">
       <div class="bg-gray-800" style="width: 320px; height: 180px;" />
       <p class="mt-2 bg-gray-800" style="width: 320px; height: 16px;" />
-      <p class="mt-2 bg-gray-800" style="width: 120px; height: 14px;" />
+      <p class="mt-2 bg-gray-800" style="width: 160px; height: 14px;" />
       <p class="mt-2 mb-4 bg-gray-800" style="width: 160px; height: 14px;" />
     </div>
     <!-- モバイル -->
     <div v-else>
-      <div class="bg-gray-800" style="width: 370px; height: 180px;" />
-      <p class="mt-2 bg-gray-800 pl-2" style="width: 370px; height: 16px;" />
-      <p class="mt-2 bg-gray-800 pl-2" style="width: 120px; height: 14px;" />
-      <p class="mt-2 mb-4 bg-gray-800 pl-2" style="width: 160px; height: 14px;" />
+      <div class="bg-gray-800" style="width: 370px; height: 208px;" />
+      <p class="mt-2 bg-gray-800 mx-2" style="width: 370px; height: 16px;" />
+      <p class="mt-2 bg-gray-800 ml-2" style="width: 160px; height: 14px;" />
+      <p class="mt-2 mb-4 bg-gray-800 ml-2" style="width: 160px; height: 14px;" />
     </div>
   </section>
 </template>
