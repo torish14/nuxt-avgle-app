9cc117fc1a3a09b40e21afcfbc9fee6acb0398d7
toshihirokato <nanamin.104@icloud.com>
2021-03-11 03:20:03 +0900

    スケルトンコンポーネントのレイアウト調整（画像のみ）

--- components/SearchSkeleton.vue
+++ components/SearchSkeleton.vue
@@ -1,6 +1,7 @@
 <template>
   <div v-if="$device.isMobile">
-    <div class="bg-gray-800 m-4" style="width: 170px; height: 96px;" />
+    <div class="bg-gray-800" style="width: 170px; height: 96px;" />
+    <!-- <div class="bg-gray-800 m-4" style="width: 170px; height: 96px;" /> -->
     <!-- <div>
       <div class="bg-gray-800 mx-2" style="width: 180px; height: 14px;" />
       <div class="mt-2 bg-gray-800 ml-2" style="width: 140px; height: 12px;" />
