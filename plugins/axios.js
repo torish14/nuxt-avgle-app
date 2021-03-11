794d1741dfd4791f1ef940c1f1076c975edcef17
toshihirokato <nanamin.104@icloud.com>
2021-03-12 04:00:47 +0900

    throttle でリクエスト制限

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,4 @@
-import { cacheAdapterEnhancer } from 'axios-extensions'
+import { cacheAdapterEnhancer, throttleAdapterEnhancer } from 'axios-extensions'
 import LRU from 'lru-cache'
 
 const ONE_HOUR = 1000 * 60 * 60
@@ -8,11 +8,13 @@ export default function ({ $axios, ssrContext }) {
     const defaultCache = process.server
       ? ssrContext.$axCache
       : new LRU({ maxAge: ONE_HOUR })
-    $axios.defaults.adapter = cacheAdapterEnhancer($axios.defaults.adapter, {
-      enabledByDefault: false,
-      cacheFlag: 'useCache',
-      defaultCache
-    })
+    $axios.defaults.adapter = throttleAdapterEnhancer(
+      cacheAdapterEnhancer($axios.defaults.adapter, {
+        enabledByDefault: false,
+        cacheFlag: 'useCache',
+        defaultCache,
+      })
+    )
     // $axios.onResponse(() => {
     //   $axios.setHeader('Access-Control-Allow-Origin', 'https://api.avgle.com/v1/search/')
     // })
