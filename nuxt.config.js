67d8fbc90606ca1bccba14b6493c3f71112da96b
toshihirokato <nanamin.104@icloud.com>
2021-03-16 08:26:05 +0900

    assets のキャッシュを有効

--- nuxt.config.js
+++ nuxt.config.js
@@ -289,8 +289,11 @@ export default {
         },
       },
       static: {
-        maxAge: 60 * 60 * 24 * 365 * 1000,
+        maxAge: 60 * 60 * 24 * 365 * 1000
       },
+      assets: {
+        maxAge: 60 * 60 * 24 * 365 * 1000
+      }
     },
     extend(config) {
       const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
