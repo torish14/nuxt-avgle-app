8cc80b2059de3fb91a7ba829fe95875199a434e3
toshihirokato <nanamin.104@icloud.com>
2021-03-16 12:01:22 +0900

    サーチスケルトンコンポーネントのレイアウト調整

--- components/SearchSkeletonGood.vue
+++ components/SearchSkeletonGood.vue
@@ -2,7 +2,7 @@
   <div v-if="$device.isMobile">
     <div>
       <!-- <div class="bg-gray-800 mx-2" style="width: 180px; height: 14px;" /> -->
-      <div class="mt-2 bg-gray-800 ml-2" style="width: 160px; height: 12px;" />
+      <div class="bg-gray-800 mt-2 ml-2" style="width: 160px; height: 12px;" />
       <!-- <div class="mt-2 mb-4 bg-gray-800 ml-2" style="width: 160px; height: 10px;" /> -->
     </div>
   </div>
--- components/SearchSkeletonTitle.vue
+++ components/SearchSkeletonTitle.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-if="$device.isMobile">
     <div>
-      <div class="bg-gray-800 mx-2" style="width: 180px; height: 14px;" />
+      <div class="bg-gray-800 mt-2 ml-2" style="width: 175px; height: 14px;" />
+      <div class="bg-gray-800 mt-2 ml-2" style="width: 160px; height: 12px;" />
       <!-- <div class="mt-2 bg-gray-800 ml-2" style="width: 160px; height: 12px;" /> -->
       <!-- <div class="mt-2 mb-4 bg-gray-800 ml-2" style="width: 160px; height: 10px;" /> -->
     </div>
