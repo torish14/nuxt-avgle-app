91ca9903f2f6d4f50afb7956675067daf05800ee
toshihirokato <nanamin.104@icloud.com>
2021-03-24 02:05:16 +0900

    eslint のルール追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,12 @@ module.exports = {
     'vue/comment-directive': 0,
     'no-console': 0,
     quotes: ['error', 'single'],
+    semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+    'semi-spacing': ['error', { after: true, before: false }],
+    'semi-style': ['error', 'first'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
     'vue/no-parsing-error': [
       'error',
       {
