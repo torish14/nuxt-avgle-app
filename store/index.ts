ca11754ff9fc2dae46d3d2b050c044d55c5aeea2
toshihirokato <nanamin.104@icloud.com>
2021-03-30 14:47:55 +0900

    不要なコードの削除

--- store/index.ts
+++ store/index.ts
@@ -1,103 +1,45 @@
-import { getAccessorType, getterTree, mutationTree, actionTree } from 'typed-vuex'
+import {
+  getAccessorType,
+  getterTree,
+  mutationTree,
+  actionTree,
+} from 'typed-vuex'
 
 import * as search from '~/store/search'
-
-// const searchUrl = 'https://api.avgle.com/v1/search/'
 
 export const state = () => ({
   currentIndexPage: 1 as number,
   currentIdPage: 1 as number,
-  prevRoute: [] as object
-  //   soaringMessages: [],
-  //   famousMessages: [],
-  //   actressMessages: [],
-  //   genreMessages: []
+  prevRoute: [] as object,
 })
 
 export type RootState = ReturnType<typeof state>
 
 export const getters = getterTree(state, {
-  currentIndexPage: state => state.currentIndexPage,
-  currentIdPage: state => state.currentIdPage,
-  prevRoute: state => state.prevRoute
-//   soaringMessages: state => state.soaringMessages,
-//   famousMessages: state => state.famousMessages,
-//   actressMessages: state => state.actressMessages,
-//   genreMessages: state => state.genreMessages
+  currentIndexPage: (state) => state.currentIndexPage,
+  currentIdPage: (state) => state.currentIdPage,
+  prevRoute: (state) => state.prevRoute,
 })
 
 export const mutations = mutationTree(state, {
-  setCurrentIndexPage (state, currentIndexPage) {
+  setCurrentIndexPage(state, currentIndexPage) {
     state.currentIndexPage = currentIndexPage
   },
-  setCurrentIdPage (state, currentIdPage) {
+  setCurrentIdPage(state, currentIdPage) {
     state.currentIdPage = currentIdPage
   },
-  changeIndexPagination (state) {
+  changeIndexPagination(state) {
     state.currentIndexPage = 1
   },
-  changeIdPagination (state) {
+  changeIdPagination(state) {
     state.currentIdPage = 1
   },
   setPrevRoute(state, from) {
     state.prevRoute = from
-  }
-//   // ? 日本で急上昇
-//   setSoaringItems (state, soaringMessages) {
-//     state.soaringMessages = soaringMessages
-//   },
-//   // ? 日本で一番人気
-//   setFamousItems (state, famousMessages) {
-//     state.famousMessages = famousMessages
-//   },
-//   // ? おすすめ女優
-//   setActressItems (state, actressMessages) {
-//     state.actressMessages = actressMessages
-//   },
-//   // ? おすすめジャンル
-//   setGenreItems (state, genreMessages) {
-//     state.genreMessages = genreMessages
-//   }
+  },
 })
 
-export const actions = actionTree({ state, getters, mutations }, {
-//   // ? メイン動画の表示
-//   async getIndexItems ({ commit }) {
-//     try {
-//       const [
-//         getSoaringItemsResponse,
-//         getFamousItemsResponse,
-//         getActressItemsResponse,
-//         getGenreItemsResponse
-//       ] = await Promise.all([
-//         // ? 日本で急上昇
-//         this.$axios.$get(
-//           encodeURI(searchUrl + '可愛い' + '/0' + '?limit=5' + '?o=tf')
-//         ),
-//         // ? 日本で一番人気
-//         this.$axios.$get(
-//           encodeURI(searchUrl + '女子高生' + '/0' + '?limit=5' + '?o=tr')
-//         ),
-//         // ? おすすめ女優
-//         this.$axios.$get(encodeURI(searchUrl + '君島みお' + '/0' + '?limit=5')),
-//         // ? おすすめジャンル
-//         this.$axios.$get(encodeURI(searchUrl + '個人撮影' + '/0' + '?limit=5'))
-//       ])
-//       commit('setSoaringItems', getSoaringItemsResponse.response.videos)
-//       commit('setFamousItems', getFamousItemsResponse.response.videos)
-//       commit('setActressItems', getActressItemsResponse.response.videos)
-//       commit('setGenreItems', getGenreItemsResponse.response.videos)
-
-//       console.log(getSoaringItemsResponse);
-//       console.log(getFamousItemsResponse);
-//       console.log(getActressItemsResponse);
-//       console.log(getGenreItemsResponse);
-//     } catch (err) {
-//       console.log(err)
-//       this.$router.push('/error')
-//     }
-//   }
-})
+export const actions = actionTree({ state, getters, mutations }, {})
 
 export const accessorType = getAccessorType({
   state,
@@ -105,6 +47,6 @@ export const accessorType = getAccessorType({
   mutations,
   actions,
   modules: {
-    search
-  }
+    search,
+  },
 })
