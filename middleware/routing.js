7e31309fe1185c79c4f27a518d74783b8f80a4c7
toshihirokato <nanamin.104@icloud.com>
2021-04-15 03:13:12 +0900

    不要なファイルの削除

--- middleware/routing.js
+++
@@ -1,9 +0,0 @@
-// export default function (context) {
-//   // current route
-//   console.log('route=', context.route.name)
-//   // previous route
-//   if (process.client) {
-//     const from = context.from
-//     console.log('from=', from)
-//   }
-// }
