c347697687d6cea0c11e934054639442317cd601
toshihirokato <nanamin.104@icloud.com>
2021-04-15 12:35:42 +0900

    placeholder の色を修正

--- components/Header.vue
+++ components/Header.vue
@@ -68,7 +68,7 @@
           >
             <input
               v-model="computedGetState"
-              class="bg-gray-700 rounded-l-full w-full py-4 px-6 text-gray-400 leading-tight focus:outline-none"
+              class="bg-gray-700 rounded-l-full w-full py-4 px-6 text-gray-400 placeholder-gray-500 focus:placeholder-gray-400 leading-tight focus:outline-none"
               type="search"
               autofocus
               placeholder="Porngle を検索"
