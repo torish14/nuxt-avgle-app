2a6473a7cc945abb9dec55f91202aeae8a37fc99
toshihirokato <nanamin.104@icloud.com>
2021-04-15 15:10:15 +0900

    モバイル版のページを作成

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,168 +1,334 @@
 <template>
-  <div v-if="$device.isDesktop">
-    <main class="container mx-auto px-96">
-      <h2 class="text-xl text-gray-100">プライバシーポリシー</h2>
-      <br />
-      <div class="text-sm">
-        <h3 class="text-gray-200 font-medium underline">
+  <section>
+    <div v-if="$device.isDesktop">
+      <main class="container mx-auto px-96">
+        <h2 class="text-xl text-gray-100">プライバシーポリシー</h2>
+        <br />
+        <div class="text-sm leading-relaxed">
+          <h3 class="text-gray-200 font-medium underline">
+            お客様の対象
+          </h3>
+          <p class="text-gray-400 mt-1">
+            当社のウェブサイトは、ウェブサイトにアクセスする管轄区域の18歳未満または該当する成年（「未成年者」）を対象としておらず、未成年者によるウェブサイトの使用を禁止しています。未成年者から故意に個人情報を収集することはありません。あなたが私たちに個人情報を提供してくれた未成年者の親または法定後見人である場合は、
+          </p>
+          <p class="text-gray-400 mt-1">
+            <a
+              href="mailto:porngle.love@gmail.com"
+              class="text-purple-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              >porngle.love@gmail.com</a
+            >
+            に連絡して、その未成年者の個人情報を削除いたします。
+          </p>
+        </div>
+        <br />
+        <div class="text-sm leading-relaxed">
+          <h3 class="text-gray-200 font-medium underline">
+            お客様から取得する情報
+          </h3>
+          <p class="text-gray-400 mt-1">
+            当社は、お客様から以下の情報を取得します。
+          </p>
+          <ul class="text-gray-400 mt-1">
+            <li>氏名(ニックネームやペンネームも含む)</li>
+            <li>メールアドレス</li>
+            <li>Cookie(クッキー)を用いて生成された識別情報</li>
+            <li>
+              当社ウェブサイトの滞在時間、入力履歴、購買履歴等の当社ウェブサイトにおけるお客様の行動履歴
+            </li>
+            <li>
+              当社アプリの起動時間、入力履歴、購買履歴等の当社アプリの利用履歴
+            </li>
+            <li>お客様の位置情報</li>
+          </ul>
+        </div>
+        <br />
+        <div class="text-sm leading-relaxed">
+          <h3 class="text-gray-200 font-medium underline">
+            お客様の情報を利用する目的
+          </h3>
+          <p class="text-gray-400 mt-1">
+            当社は、お客様から取得した情報を、以下の目的のために利用します。
+          </p>
+          <ul class="text-gray-400 mt-1">
+            <li>
+              当社サービスに関する登録の受付、お客様の本人確認、認証のため
+            </li>
+            <li>お客様の当社サービスの利用履歴を管理するため</li>
+            <li>
+              当社サービスにおけるお客様の行動履歴を分析し、当社サービスの維持改善に役立てるため
+            </li>
+            <li>市場分析、マーケティングのため</li>
+            <li>広告の配信、表示及び効果測定のため</li>
+            <li>
+              お客様の趣味嗜好にあわせたターゲティング広告を表示するため
+            </li>
+            <li>当社のサービスに関するご案内をするため</li>
+            <li>提携する事業者・サービスのご案内をお送りするため</li>
+            <li>お客様からのお問い合わせに対応するため</li>
+            <li>当社の規約や法令に違反する行為に対応するため</li>
+            <li>
+              当社サービスの変更、提供中止、終了、契約解除をご連絡するため
+            </li>
+            <li>当社規約の変更等を通知するため</li>
+            <li>
+              以上の他、当社サービスの提供、維持、保護及び改善のため
+            </li>
+          </ul>
+        </div>
+        <br />
+        <div class="text-sm leading-relaxed">
+          <h3 class="text-gray-200 font-medium underline">
+            第三者提供
+          </h3>
+          <p class="text-gray-400 mt-1">
+            当社は、お客様から取得する情報のうち、個人データ（個人情報保護法第２条第６項）に該当するものついては、あらかじめお客様の同意を得ずに、第三者（日本国外にある者を含みます。）に提供しません。
+          </p>
+          <p class="text-gray-400 mt-1">但し、次の場合は除きます。</p>
+          <ul class="text-gray-400 mt-1">
+            <li>個人データの取扱いを外部に委託する場合</li>
+            <li>当社や当社サービスが買収された場合</li>
+            <li>
+              事業パートナーと共同利用する場合（具体的な共同利用がある場合は、その内容を別途公表します。）
+            </li>
+            <li>
+              その他、法律によって合法的に第三者提供が許されている場合
+            </li>
+          </ul>
+        </div>
+        <br />
+        <div class="text-sm leading-relaxed">
+          <h3 class="text-gray-200 font-medium underline">
+            アクセス解析ツール
+          </h3>
+          <p class="text-gray-400 mt-1">
+            当社は、お客様のアクセス解析のために、「Googleアナリティクス」を利用しています。Googleアナリティクスは、トラフィックデータの収集のためにCookieを使用しています。トラフィックデータは匿名で収集されており、個人を特定するものではありません。Cookieを無効にすれば、これらの情報の収集を拒否することができます。詳しくはお使いのブラウザの設定をご確認ください。Googleアナリティクスについて、詳しくは以下からご確認ください。
+          </p>
+          <p class="text-gray-400 mt-1">
+            <a
+              href="https://marketingplatform.google.com/about/analytics/terms/jp/"
+              class="text-purple-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Google アナリティクス利用規約</a
+            >
+          </p>
+        </div>
+        <br />
+        <div class="text-sm leading-relaxed">
+          <h3 class="text-gray-200 font-medium underline">
+            広告の配信
+          </h3>
+          <p class="text-gray-400 mt-1">
+            当社ウェブサイトでは、広告が表示されることがあります。第三者の広告を表示する場合にお客様から取得した情報を活用する場合は、当社ウェブサイト等で、別途その詳細を表示します。
+          </p>
+        </div>
+        <br />
+        <div class="text-sm leading-relaxed">
+          <h3 class="text-gray-200 font-medium underline">
+            プライバシーポリシーの変更
+          </h3>
+          <p class="text-gray-400 mt-1">
+            当社は、必要に応じて、このプライバシーポリシーの内容を変更します。この場合、変更後のプライバシーポリシーの施行時期と内容を適切な方法により周知または通知します。
+          </p>
+        </div>
+        <br />
+        <div class="text-sm leading-relaxed">
+          <h3 class="text-gray-200 font-medium underline">
+            お問い合わせ
+          </h3>
+          <p class="text-gray-400 mt-1">
+            お客様の情報の開示、情報の訂正、利用停止、削除をご希望の場合は、以下のメールアドレスにご連絡ください。
+          </p>
+          <br />
+          <p class="text-gray-400 mt-1">e-mail</p>
+          <p class="text-gray-400 mt-1">
+            <a
+              href="mailto:porngle.love@gmail.com"
+              class="text-purple-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              >porngle.love@gmail.com</a
+            >
+          </p>
+          <br />
+          <p class="text-gray-400 mt-1">
+            この場合、必ず、運転免許証のご提示等当社が指定する方法により、ご本人からのご請求であることの確認をさせていただきます。なお、情報の開示請求については、開示の有無に関わらず、ご申請時に一件あたり1,000円の事務手数料を申し受けます。
+          </p>
+          <br />
+          <p class="text-gray-400 mt-1">事業者の名称</p>
+          <p class="text-gray-400 mt-1">Porngle</p>
+          <br />
+          <p class="text-gray-400 mt-1">2021年04月09日 制定</p>
+        </div>
+      </main>
+    </div>
+    <div v-else class="pt-1 pb-20">
+      <main class="container mx-auto px-8">
+        <h2 class="text-base text-gray-100">プライバシーポリシー</h2>
+        <br />
+        <h3 class="text-gray-200 text-sm font-medium underline">
           お客様の対象
         </h3>
-        <p class="text-gray-400 mt-1">
-          当社のウェブサイトは、ウェブサイトにアクセスする管轄区域の18歳未満または該当する成年（「未成年者」）を対象としておらず、未成年者によるウェブサイトの使用を禁止しています。未成年者から故意に個人情報を収集することはありません。あなたが私たちに個人情報を提供してくれた未成年者の親または法定後見人である場合は、
-        </p>
-        <p class="text-gray-400 mt-1">
-          <a
-            href="mailto:porngle.love@gmail.com"
-            class="text-purple-500"
-            target="_blank"
-            rel="noopener noreferrer"
-            >porngle.love@gmail.com</a
-          >
-          に連絡して、その未成年者の個人情報を削除いたします。
-        </p>
-      </div>
-      <br />
-      <div class="text-sm">
-        <h3 class="text-gray-200 font-medium underline">
+        <div class="text-xs leading-relaxed">
+          <p class="text-gray-400 mt-1">
+            当社のウェブサイトは、ウェブサイトにアクセスする管轄区域の18歳未満または該当する成年（「未成年者」）を対象としておらず、未成年者によるウェブサイトの使用を禁止しています。未成年者から故意に個人情報を収集することはありません。あなたが私たちに個人情報を提供してくれた未成年者の親または法定後見人である場合は、
+          </p>
+          <p class="text-gray-400 mt-1">
+            <a
+              href="mailto:porngle.love@gmail.com"
+              class="text-purple-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              >porngle.love@gmail.com</a
+            >
+            に連絡して、その未成年者の個人情報を削除いたします。
+          </p>
+        </div>
+        <br />
+        <h3 class="text-gray-200 text-sm font-medium underline">
           お客様から取得する情報
         </h3>
-        <p class="text-gray-400 mt-1">
-          当社は、お客様から以下の情報を取得します。
-        </p>
-        <ul class="text-gray-400 mt-1">
-          <li>氏名(ニックネームやペンネームも含む)</li>
-          <li>メールアドレス</li>
-          <li>Cookie(クッキー)を用いて生成された識別情報</li>
-          <li>
-            当社ウェブサイトの滞在時間、入力履歴、購買履歴等の当社ウェブサイトにおけるお客様の行動履歴
-          </li>
-          <li>
-            当社アプリの起動時間、入力履歴、購買履歴等の当社アプリの利用履歴
-          </li>
-          <li>お客様の位置情報</li>
-        </ul>
-      </div>
-      <br />
-      <div class="text-sm">
-        <h3 class="text-gray-200 font-medium underline">
+        <div class="text-xs leading-relaxed">
+          <p class="text-gray-400 mt-1">
+            当社は、お客様から以下の情報を取得します。
+          </p>
+          <ul class="text-gray-400 mt-1">
+            <li>氏名(ニックネームやペンネームも含む)</li>
+            <li>メールアドレス</li>
+            <li>Cookie(クッキー)を用いて生成された識別情報</li>
+            <li>
+              当社ウェブサイトの滞在時間、入力履歴、購買履歴等の当社ウェブサイトにおけるお客様の行動履歴
+            </li>
+            <li>
+              当社アプリの起動時間、入力履歴、購買履歴等の当社アプリの利用履歴
+            </li>
+            <li>お客様の位置情報</li>
+          </ul>
+        </div>
+        <br />
+        <h3 class="text-gray-200 text-sm font-medium underline">
           お客様の情報を利用する目的
         </h3>
-        <p class="text-gray-400 mt-1">
-          当社は、お客様から取得した情報を、以下の目的のために利用します。
-        </p>
-        <ul class="text-gray-400 mt-1">
-          <li>
-            当社サービスに関する登録の受付、お客様の本人確認、認証のため
-          </li>
-          <li>お客様の当社サービスの利用履歴を管理するため</li>
-          <li>
-            当社サービスにおけるお客様の行動履歴を分析し、当社サービスの維持改善に役立てるため
-          </li>
-          <li>市場分析、マーケティングのため</li>
-          <li>広告の配信、表示及び効果測定のため</li>
-          <li>
-            お客様の趣味嗜好にあわせたターゲティング広告を表示するため
-          </li>
-          <li>当社のサービスに関するご案内をするため</li>
-          <li>提携する事業者・サービスのご案内をお送りするため</li>
-          <li>お客様からのお問い合わせに対応するため</li>
-          <li>当社の規約や法令に違反する行為に対応するため</li>
-          <li>
-            当社サービスの変更、提供中止、終了、契約解除をご連絡するため
-          </li>
-          <li>当社規約の変更等を通知するため</li>
-          <li>
-            以上の他、当社サービスの提供、維持、保護及び改善のため
-          </li>
-        </ul>
-      </div>
-      <br />
-      <div class="text-sm">
-        <h3 class="text-gray-200 font-medium underline">
+        <div class="text-xs leading-relaxed">
+          <p class="text-gray-400 mt-1">
+            当社は、お客様から取得した情報を、以下の目的のために利用します。
+          </p>
+          <ul class="text-gray-400 mt-1">
+            <li>
+              当社サービスに関する登録の受付、お客様の本人確認、認証のため
+            </li>
+            <li>お客様の当社サービスの利用履歴を管理するため</li>
+            <li>
+              当社サービスにおけるお客様の行動履歴を分析し、当社サービスの維持改善に役立てるため
+            </li>
+            <li>市場分析、マーケティングのため</li>
+            <li>広告の配信、表示及び効果測定のため</li>
+            <li>
+              お客様の趣味嗜好にあわせたターゲティング広告を表示するため
+            </li>
+            <li>当社のサービスに関するご案内をするため</li>
+            <li>提携する事業者・サービスのご案内をお送りするため</li>
+            <li>お客様からのお問い合わせに対応するため</li>
+            <li>当社の規約や法令に違反する行為に対応するため</li>
+            <li>
+              当社サービスの変更、提供中止、終了、契約解除をご連絡するため
+            </li>
+            <li>当社規約の変更等を通知するため</li>
+            <li>
+              以上の他、当社サービスの提供、維持、保護及び改善のため
+            </li>
+          </ul>
+        </div>
+        <br />
+        <h3 class="text-gray-200 text-sm font-medium underline">
           第三者提供
         </h3>
-        <p class="text-gray-400 mt-1">
-          当社は、お客様から取得する情報のうち、個人データ（個人情報保護法第２条第６項）に該当するものついては、あらかじめお客様の同意を得ずに、第三者（日本国外にある者を含みます。）に提供しません。
-        </p>
-        <p class="text-gray-400 mt-1">但し、次の場合は除きます。</p>
-        <ul class="text-gray-400 mt-1">
-          <li>個人データの取扱いを外部に委託する場合</li>
-          <li>当社や当社サービスが買収された場合</li>
-          <li>
-            事業パートナーと共同利用する場合（具体的な共同利用がある場合は、その内容を別途公表します。）
-          </li>
-          <li>
-            その他、法律によって合法的に第三者提供が許されている場合
-          </li>
-        </ul>
-      </div>
-      <br />
-      <div class="text-sm">
-        <h3 class="text-gray-200 font-medium underline">
+        <div class="text-xs leading-relaxed">
+          <p class="text-gray-400 mt-1">
+            当社は、お客様から取得する情報のうち、個人データ（個人情報保護法第２条第６項）に該当するものついては、あらかじめお客様の同意を得ずに、第三者（日本国外にある者を含みます。）に提供しません。
+          </p>
+          <p class="text-gray-400 mt-1">但し、次の場合は除きます。</p>
+          <ul class="text-gray-400 mt-1">
+            <li>個人データの取扱いを外部に委託する場合</li>
+            <li>当社や当社サービスが買収された場合</li>
+            <li>
+              事業パートナーと共同利用する場合（具体的な共同利用がある場合は、その内容を別途公表します。）
+            </li>
+            <li>
+              その他、法律によって合法的に第三者提供が許されている場合
+            </li>
+          </ul>
+        </div>
+        <br />
+        <h3 class="text-gray-200 text-sm font-medium underline">
           アクセス解析ツール
         </h3>
-        <p class="text-gray-400 mt-1">
-          当社は、お客様のアクセス解析のために、「Googleアナリティクス」を利用しています。Googleアナリティクスは、トラフィックデータの収集のためにCookieを使用しています。トラフィックデータは匿名で収集されており、個人を特定するものではありません。Cookieを無効にすれば、これらの情報の収集を拒否することができます。詳しくはお使いのブラウザの設定をご確認ください。Googleアナリティクスについて、詳しくは以下からご確認ください。
-        </p>
-        <p class="text-gray-400 mt-1">
-          <a
-            href="https://marketingplatform.google.com/about/analytics/terms/jp/"
-            class="text-purple-500"
-            target="_blank"
-            rel="noopener noreferrer"
-            >https://marketingplatform.google.com/about/analytics/terms/jp/</a
-          >
-        </p>
-      </div>
-      <br />
-      <div class="text-sm">
-        <h3 class="text-gray-200 font-medium underline">
+        <div class="text-xs leading-relaxed">
+          <p class="text-gray-400 mt-1">
+            当社は、お客様のアクセス解析のために、「Googleアナリティクス」を利用しています。Googleアナリティクスは、トラフィックデータの収集のためにCookieを使用しています。トラフィックデータは匿名で収集されており、個人を特定するものではありません。Cookieを無効にすれば、これらの情報の収集を拒否することができます。詳しくはお使いのブラウザの設定をご確認ください。Googleアナリティクスについて、詳しくは以下からご確認ください。
+          </p>
+          <p class="text-gray-400 mt-1">
+            <a
+              href="https://marketingplatform.google.com/about/analytics/terms/jp/"
+              class="text-purple-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Google アナリティクス利用規約</a
+            >
+          </p>
+        </div>
+        <br />
+        <h3 class="text-gray-200 text-sm font-medium underline">
           広告の配信
         </h3>
-        <p class="text-gray-400 mt-1">
-          当社ウェブサイトでは、広告が表示されることがあります。第三者の広告を表示する場合にお客様から取得した情報を活用する場合は、当社ウェブサイト等で、別途その詳細を表示します。
-        </p>
-      </div>
-      <br />
-      <div class="text-sm">
-        <h3 class="text-gray-200 font-medium underline">
+        <div class="text-xs leading-relaxed">
+          <p class="text-gray-400 mt-1">
+            当社ウェブサイトでは、広告が表示されることがあります。第三者の広告を表示する場合にお客様から取得した情報を活用する場合は、当社ウェブサイト等で、別途その詳細を表示します。
+          </p>
+        </div>
+        <br />
+        <h3 class="text-gray-200 text-sm font-medium underline">
           プライバシーポリシーの変更
         </h3>
-        <p class="text-gray-400 mt-1">
-          当社は、必要に応じて、このプライバシーポリシーの内容を変更します。この場合、変更後のプライバシーポリシーの施行時期と内容を適切な方法により周知または通知します。
-        </p>
-      </div>
-      <br />
-      <div class="text-sm">
-        <h3 class="text-gray-200 font-medium underline">
+        <div class="text-xs leading-relaxed">
+          <p class="text-gray-400 mt-1">
+            当社は、必要に応じて、このプライバシーポリシーの内容を変更します。この場合、変更後のプライバシーポリシーの施行時期と内容を適切な方法により周知または通知します。
+          </p>
+        </div>
+        <br />
+        <h3 class="text-gray-200 text-sm font-medium underline">
           お問い合わせ
         </h3>
-        <p class="text-gray-400 mt-1">
-          お客様の情報の開示、情報の訂正、利用停止、削除をご希望の場合は、以下のメールアドレスにご連絡ください。
-        </p>
-        <br />
-        <p class="text-gray-400 mt-1">e-mail</p>
-        <p class="text-gray-400 mt-1">
-          <a
-            href="mailto:porngle.love@gmail.com"
-            class="text-purple-500"
-            target="_blank"
-            rel="noopener noreferrer"
-            >porngle.love@gmail.com</a
-          >
-        </p>
-        <br />
-        <p class="text-gray-400 mt-1">
-          この場合、必ず、運転免許証のご提示等当社が指定する方法により、ご本人からのご請求であることの確認をさせていただきます。なお、情報の開示請求については、開示の有無に関わらず、ご申請時に一件あたり1,000円の事務手数料を申し受けます。
-        </p>
-        <br />
-        <p class="text-gray-400 mt-1">事業者の名称</p>
-        <p class="text-gray-400 mt-1">Porngle</p>
-        <br />
-        <p class="text-gray-400 mt-1">2021年04月09日 制定</p>
-      </div>
-    </main>
-  </div>
+        <div class="text-xs leading-relaxed">
+          <p class="text-gray-400 mt-1">
+            お客様の情報の開示、情報の訂正、利用停止、削除をご希望の場合は、以下のメールアドレスにご連絡ください。
+          </p>
+          <br />
+          <p class="text-gray-400 mt-1">e-mail</p>
+          <p class="text-gray-400 mt-1">
+            <a
+              href="mailto:porngle.love@gmail.com"
+              class="text-purple-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              >porngle.love@gmail.com</a
+            >
+          </p>
+          <br />
+          <p class="text-gray-400 mt-1">
+            この場合、必ず、運転免許証のご提示等当社が指定する方法により、ご本人からのご請求であることの確認をさせていただきます。なお、情報の開示請求については、開示の有無に関わらず、ご申請時に一件あたり1,000円の事務手数料を申し受けます。
+          </p>
+          <br />
+          <p class="text-gray-400 mt-1">事業者の名称</p>
+          <p class="text-gray-400 mt-1">Porngle</p>
+          <br />
+          <p class="text-gray-400 mt-1">2021年04月09日 制定</p>
+        </div>
+      </main>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
