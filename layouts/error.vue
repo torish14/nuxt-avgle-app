a6cff026e2240fed837e1e48cb2d5e457d87a703
toshihirokato <nanamin.104@icloud.com>
2021-04-17 02:19:48 +0900

    モバイル版のレイアウトを追加

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,41 +1,80 @@
 <template>
-  <div class="error bg-gray-800">
-    <div class="flex flex-col flex-shrink-0 items-center pt-12">
-      <template v-if="error.statusCode === 404">
-        <img
-          src="~/assets/Page_not_found.jpeg"
-          alt="残念ながら、お探しのページが見つかりませんでした。"
-          width="400"
-          height="393"
-        />
-        <p class="text-gray-200 text-base">
-          &nbsp;お探しのページが見つかりませんでした。
-        </p>
-      </template>
-      <template v-else class="text-gray-400 text-base">
-        <img
-          src="~/assets/Error.jpeg"
-          alt="申し訳ございません。エラーが発生しました。"
-          width="400"
-          height="393"
-        />
-        <p class="text-gray-200 text-sm">
-          <!-- {{ error.statusCode }} -->
-          <!-- {{ error.message }} -->
-          &nbsp;エラーが発生しました。
-        </p>
-      </template>
-      <!-- prettier-ignore -->
-      <nuxt-link
-        to="/"
-        aria-label="ホームへ戻る"
-        class="text-gray-400 text-sm hover:text-purple-500"
-        @click.native="setJapaneseForm(); refresh()"
-      >
-        &nbsp;トップページに戻る
-      </nuxt-link>
+  <section>
+    <div v-if="$device.isDesktop">
+      <div class="flex flex-col flex-shrink-0 items-center pt-12">
+        <template v-if="error.statusCode === 404">
+          <img
+            src="~/assets/Page_not_found.jpeg"
+            alt="残念ながら、お探しのページが見つかりませんでした。"
+            width="400"
+            height="393"
+          />
+          <p class="text-gray-200 text-base">
+            &nbsp;お探しのページが見つかりませんでした。
+          </p>
+        </template>
+        <template v-else>
+          <img
+            src="~/assets/Error.jpeg"
+            alt="申し訳ございません。エラーが発生しました。"
+            width="400"
+            height="393"
+          />
+          <p class="text-gray-200 text-sm">
+            <!-- {{ error.statusCode }} -->
+            <!-- {{ error.message }} -->
+            &nbsp;エラーが発生しました。
+          </p>
+        </template>
+        <!-- prettier-ignore -->
+        <nuxt-link
+          to="/"
+          aria-label="ホームへ戻る"
+          class="text-gray-400 text-sm hover:text-purple-500"
+          @click.native="setJapaneseForm(); refresh()"
+        >
+          &nbsp;トップページに戻る
+        </nuxt-link>
+      </div>
     </div>
-  </div>
+    <div v-else>
+      <div class="flex flex-col flex-shrink-0 items-center pt-12">
+        <template v-if="error.statusCode === 404">
+          <img
+            src="~/assets/Page_not_found.jpeg"
+            alt="残念ながら、お探しのページが見つかりませんでした。"
+            width="250"
+            height="223"
+          />
+          <p class="text-gray-200 text-base">
+            &nbsp;お探しのページが見つかりませんでした。
+          </p>
+        </template>
+        <template v-else>
+          <img
+            src="~/assets/Error.jpeg"
+            alt="申し訳ございません。エラーが発生しました。"
+            width="250"
+            height="223"
+          />
+          <p class="text-gray-200 text-sm">
+            <!-- {{ error.statusCode }} -->
+            <!-- {{ error.message }} -->
+            &nbsp;エラーが発生しました。
+          </p>
+        </template>
+        <!-- prettier-ignore -->
+        <nuxt-link
+          to="/"
+          aria-label="ホームへ戻る"
+          class="text-gray-400 text-sm hover:text-purple-500"
+          @click.native="setJapaneseForm(); refresh()"
+        >
+          &nbsp;トップページに戻る
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -43,7 +82,7 @@ import Vue from 'vue'
 import Meta from '~/assets/mixins/meta'
 
 export type DataType = {
-  meta: object
+  meta: string
 }
 
 export default Vue.extend({
@@ -76,8 +115,4 @@ export default Vue.extend({
 })
 </script>
 
-<style>
-.error {
-  padding-top: 4px;
-}
-</style>
+<style></style>
