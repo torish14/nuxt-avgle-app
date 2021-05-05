a8eec1b0a9be066e5da4de629e9b3e12ce63a076
toshihirokato <nanamin.104@icloud.com>
2021-05-05 17:27:33 +0900

    account.vue を mypage.vue に修正

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -10,64 +10,7 @@
             <nuxt-link to="/" aria-label="ホームへ戻る">
               <i class="material-icons m-2">close</i>
             </nuxt-link>
-            <h1 class="text-base m-2">アカウント</h1>
-          </div>
-          <div class="flex justify-center m-4">
-            <img
-              src="~/assets/Marilyn.jpeg"
-              alt="マリリン・モンロー"
-              width="250"
-              height="223"
-              style="width: 250px; height: 223px"
-            />
-          </div>
-          <div class="flex flex-col items-center">
-            <h2 class="text-gray-200 text-sm">Porngle を使い倒す</h2>
-            <h3
-              class="container mx-auto px-16 mt-2 text-gray-400 text-xs"
-            >
-              視聴履歴の確認やいいね、コメントの投稿を
-            </h3>
-            <h3 class="text-gray-400 text-xs mt-1">
-              するにはログインしてください。
-            </h3>
-          </div>
-          <div class="flex justify-center mt-4">
-            <button
-              class="bg-blue-500 hover:bg-blue-dark text-gray-800 font-semibold py-1.5 px-3 rounded"
-              type="button"
-              @click="signInWithGoogle"
-            >
-              ログイン
-            </button>
-          </div>
-          <nuxt-link to="/help" aria-label="ヘルプへ進む">
-            <div class="flex text-gray-300 mt-16">
-              <i class="material-icons m-2">help</i>
-              <p class="text-sm m-2 pt-1">ヘルプ</p>
-            </div>
-          </nuxt-link>
-          <footer class="mt-auto mb-4">
-            <div class="flex justify-center text-gray-400 text-xs">
-              <nuxt-link to="/terms" aria-label="利用規約へ進む"
-                >利用規約・</nuxt-link
-              >
-              <nuxt-link
-                to="/privacy"
-                aria-label="プライバシーポリシーへ進む"
-                >プライバシーポリシー</nuxt-link
-              >
-            </div>
-          </footer>
-        </div>
-      </template>
-      <template v-else>
-        <div class="flex flex-col min-h-screen">
-          <div class="flex text-gray-200">
-            <nuxt-link to="/" aria-label="ホームへ戻る">
-              <i class="material-icons m-2">close</i>
-            </nuxt-link>
-            <h1 class="text-base m-2">アカウント</h1>
+            <h1 class="text-lg font-semibold m-2">マイページ</h1>
           </div>
           <div
             v-for="(data, index) in getUsers"
@@ -96,7 +39,64 @@
           <nuxt-link to="/help" aria-label="ヘルプへ進む">
             <div class="flex text-gray-300 mt-16">
               <i class="material-icons m-2">help</i>
-              <p class="text-sm m-2 pt-1">ヘルプ</p>
+              <p class="text-base m-2">ヘルプ</p>
+            </div>
+          </nuxt-link>
+          <footer class="mt-auto mb-4">
+            <div class="flex justify-center text-gray-400 text-xs">
+              <nuxt-link to="/terms" aria-label="利用規約へ進む"
+                >利用規約・</nuxt-link
+              >
+              <nuxt-link
+                to="/privacy"
+                aria-label="プライバシーポリシーへ進む"
+                >プライバシーポリシー</nuxt-link
+              >
+            </div>
+          </footer>
+        </div>
+      </template>
+      <template v-else>
+        <div class="flex flex-col min-h-screen">
+          <div class="flex text-gray-200">
+            <nuxt-link to="/" aria-label="ホームへ戻る">
+              <i class="material-icons m-2">close</i>
+            </nuxt-link>
+            <h1 class="text-lg font-semibold m-2">マイページ</h1>
+          </div>
+          <div class="flex justify-center m-4">
+            <img
+              src="~/assets/Marilyn.jpeg"
+              alt="マリリン・モンロー"
+              width="250"
+              height="223"
+              style="width: 250px; height: 223px"
+            />
+          </div>
+          <div class="flex flex-col items-center">
+            <h2 class="text-gray-200 text-base">
+              Porngle を使い倒す
+            </h2>
+            <h3
+              class="container mx-auto px-16 mt-2 text-gray-400 text-base text-center"
+            >
+              視聴履歴の確認やいいね、コメントの投稿をするにはログインしてください。
+            </h3>
+            <!-- <h3 class="text-gray-400 text-base mt-1"></h3> -->
+          </div>
+          <div class="flex justify-center mt-4">
+            <button
+              class="bg-blue-500 hover:bg-blue-dark text-gray-800 font-semibold py-1.5 px-3 rounded"
+              type="button"
+              @click="signInWithGoogle"
+            >
+              ログイン
+            </button>
+          </div>
+          <nuxt-link to="/help" aria-label="ヘルプへ進む">
+            <div class="flex text-gray-300 mt-16">
+              <i class="material-icons m-2">help</i>
+              <p class="text-base m-2">ヘルプ</p>
             </div>
           </nuxt-link>
           <footer class="mt-auto mb-4">
@@ -124,6 +124,7 @@ import Meta from '~/assets/mixins/meta'
 
 export type DataType = {
   meta: object
+  isLogin: boolean
 }
 
 export default Vue.extend({
@@ -133,25 +134,26 @@ export default Vue.extend({
   data(): DataType {
     return {
       meta: {
-        title: 'アカウント',
-        description: 'アカウントページです。',
+        title: 'マイページ',
+        description: 'マイページです。',
         type: 'website',
-        url: 'https://porngle.love/account',
+        url: 'https://porngle.love/mypage',
         image: 'https://porngle.love/assets/PG.jpeg',
         robots: 'noindex',
       },
+      isLogin: false,
     }
   },
   computed: {
     ...mapGetters('auth', ['getUsers']),
-    isLogin() {
-      return this.$fire.auth.onAuthStateChanged
-    },
   },
   created(): void {
     this.$fire.auth.onAuthStateChanged((user): void => {
       if (user) {
         this.$accessor.auth.fetchUsers()
+        this.isLogin = true
+      } else {
+        this.isLogin = false
       }
     })
   },
